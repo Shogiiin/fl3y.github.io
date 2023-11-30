769b1ffb5aef5856f10889f42b640ffb4cabc368
Fl3y <loftizockt.gd@gmail.com>
2023-11-30 22:07:04 +0100

    impl(web): TailwindCss added, added default page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   app: {
     baseURL: "/fl3y.github.io/",
   },
+  runtimeConfig: {
+    apiSecret: "1",
+  },
+
+  modules: ["@nuxtjs/tailwindcss"],
 });
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="underline text-center  text-4xl">ImFley!</h1>
+    </div>
+    <AppAlert>
+        This is an auto-imported component
+    </AppAlert>
+    <div>
+        <h2>
+            Test
+        </h2>
+    </div>
+</template>

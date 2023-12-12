e6c523d91b93c62867d275aef727c1aa95b750af
Fl3y <loftizockt.gd@gmail.com>
2023-12-12 01:15:24 +0100

    TODO: Recreate Main Page

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,14 +1,14 @@
 <template>
     <nav class="flex items-center justify-between flex-wrap  bg-purple-700 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
-            <span class=" font-semibold text-x1 tracking-tight">Tailwind Nav</span>
+            <span class=" font-semibold text-x1 tracking-tight">FleySite</span>
         </div>
         <div class="block lg:hidden">
-        </div>  
+        </div>
         <div class="w-full block flex-glow lg:flex lg:items-center lg:w-auto">
             <div class="text-sm lg:flex-glow">
-                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                    Docs
+                <a href="about" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                    About
                 </a>
                 <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                     Examples
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,11 @@
 <script setup lang="ts">
-    definePageMeta({
-        layout:"default"
-    })
+definePageMeta({
+    layout: "default"
+})
 </script>
 
 <template>
-    <div>
-        <h1 class="underline text-center  text-4xl">ImFley!</h1>
-
-    </div>
-
-    <Card>
-        <div class="font-bold text-xl mb-2">
-            Thats me
-        </div>
-        <p class="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dolores reprehenderit numquam, debitis excepturi libero pariatur quibusdam eos animi porro minus totam minima. Tenetur consequatur nisi quis ullam molestias voluptates.
-        </p>
-    </Card>
-
-    <AppAlert>
-        This is an auto-imported component
-    </AppAlert>
-    <div>
-        <h2>
-            Test
-        </h2>
-    </div>
+    <h1 class="">
+        WELCOME
+    </h1>
 </template>
